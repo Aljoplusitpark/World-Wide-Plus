ec1eb3954d437435a4ff998c961cb710f2e52874
Vyshnaviprasee <130043651+Vyshnaviprasee@users.noreply.github.com>
2023-10-25 17:51:57 +0530

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -900,5 +900,35 @@ $(".button-prev").click(function() {
   $(e).removeClass("active"), $(e).prev().addClass("active"), $(".feature-slide").hasClass("active") || $(".feature-slide:last").addClass("active")
 });
 
+// technology
+
+
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const tabId = tab.getAttribute('id').replace('tab', 'content');
+
+    tabs.forEach(tab => tab.classList.remove('active'));
+    tabContents.forEach(content => content.style.display = 'none');
+
+    tab.classList.add('active');
+    document.getElementById(tabId).style.display = 'block';
+  });
+});
+
+const tabs = document.querySelectorAll('.tab');
+const tabContents = document.querySelectorAll('.tab-content');
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const tabId = tab.getAttribute('id').replace('tab', 'content');
+
+    tabs.forEach(tab => tab.classList.remove('active'));
+    tabContents.forEach(content => content.style.display = 'none');
+
+    tab.classList.add('active');
+    document.getElementById(tabId).style.display = 'block';
+  });
+});
 
 
