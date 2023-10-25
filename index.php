4d77b3c705afaeb6002284fd67c681bc9e76a88b
shyamkrishnak89 <cto@plusitpark.com>
2023-10-25 14:45:35 +0530

    y

--- index.php
+++ index.php
@@ -387,10 +387,10 @@
     <div class="cs-height_90 cs-height_lg_40"></div>
     <div class="text-center">
       <a href="#" class="cs-text_btn">
-        <span>Load More</span>
-        <svg width="26" height="12" viewBox="0 0 26 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M25.5303 6.53033C25.8232 6.23744 25.8232 5.76256 25.5303 5.46967L20.7574 0.696699C20.4645 0.403806 19.9896 0.403806 19.6967 0.696699C19.4038 0.989593 19.4038 1.46447 19.6967 1.75736L23.9393 6L19.6967 10.2426C19.4038 10.5355 19.4038 11.0104 19.6967 11.3033C19.9896 11.5962 20.4645 11.5962 20.7574 11.3033L25.5303 6.53033ZM0 6.75H25V5.25H0V6.75Z" fill="currentColor"></path>
-        </svg>                
+        <!-- <span>Load More</span> -->
+        <!-- <svg width="26" height="12" viewBox="0 0 26 12" fill="none" xmlns="http://www.w3.org/2000/svg"> -->
+          <!-- <path d="M25.5303 6.53033C25.8232 6.23744 25.8232 5.76256 25.5303 5.46967L20.7574 0.696699C20.4645 0.403806 19.9896 0.403806 19.6967 0.696699C19.4038 0.989593 19.4038 1.46447 19.6967 1.75736L23.9393 6L19.6967 10.2426C19.4038 10.5355 19.4038 11.0104 19.6967 11.3033C19.9896 11.5962 20.4645 11.5962 20.7574 11.3033L25.5303 6.53033ZM0 6.75H25V5.25H0V6.75Z" fill="currentColor"></path> -->
+        <!-- </svg>                 -->
       </a>
     </div>
   </div><br><br><br><br>
@@ -461,15 +461,15 @@
         <div class="cs-pagination cs-style1"></div>
       </div>
       <!-- .cs-slider -->
-    </section>
+    <!-- </section> -->
     <!-- End Latest Project -->
-    <div class="cs-height_150 cs-height_lg_80"></div>
+    <!-- <div class="cs-height_150 cs-height_lg_80"></div> -->
     <!-- Start Awards Text -->
-    <section class="cs-shape_wrap_2" >
-      <div class="cs-shape_2">
-        <div></div>
-      </div>
-      <div class="container" >
+    <!-- <section class="cs-shape_wrap_2" > -->
+      <!-- <div class="cs-shape_2"> -->
+        <!-- <div></div> -->
+      <!-- </div> -->
+      <!-- <div class="container" >
         <div class="cs-slider cs-style1 cs-gap-24">
           <div class="cs-slider_left">
             <div class="cs-section_heading cs-style1">
@@ -489,29 +489,29 @@
                 </svg>
               </div>
             </div>
-          </div>
-          <div class="cs-slider_right">
+          </div> -->
+          <!-- <div class="cs-slider_right">
             <div class="cs-slider_container" data-autoplay="0" data-loop="1" data-speed="600" data-center="0" data-slides-per-view="responsive" data-xs-slides="1" data-sm-slides="2" data-md-slides="2" data-lg-slides="2" data-add-slides="2">
               <div class="cs-slider_wrapper">
                 <div class="cs-slide">
-                  <div class="cs-time_line cs-style1">
+                  <div class="cs-time_line cs-style1"> -->
                     <!-- <h3 class="cs-accent_color">2023</h3> -->
-                    <a href=""><img src="assets/img/clutch_review.webp" style="width:160px; height: auto;"></a>
+                    <!-- <a href=""><img src="assets/img/clutch_review.webp" style="width:160px; height: auto;"></a> -->
                     <!-- <h2>Cluth</h2> -->
                     <!-- <p>Website of the day</p>
                     <p>Mobile exelence</p> -->
-                  </div>
+                  <!-- </div>
                   <div class="cs-height_40 cs-height_lg_30"></div>
                   <div class="cs-time_line cs-style1">
                     <a href=""><img src="assets/img/goodfirms_review.webp" style="width:160px; height: auto;"></a>
                   </div>
-                </div>
+                </div> -->
                 <!-- .cs-slide -->
-                <div class="cs-slide">
+                <!-- <div class="cs-slide">
                   <div class="cs-time_line cs-style1">
                      <a href=""><img src="assets/img/google_review.webp" style="width:160px; height: auto;"></a>
 
-                  </div>
+                  </div> -->
                   <!-- <div class="cs-height_40 cs-height_lg_30"></div>
                   <div class="cs-time_line cs-style1">
                     <h3 class="cs-accent_color">2022</h3>
@@ -519,7 +519,7 @@
                     <p>Website of the day</p>
                     <p>Mobile exelence</p>
                   </div> -->
-                </div>
+                <!-- </div> -->
                 <!-- .cs-slide -->
                 <!-- <div class="cs-slide">
                   <div class="cs-time_line cs-style1">
@@ -553,25 +553,25 @@
                   </div>
                 </div> -->
                 <!-- .cs-slide -->
-              </div>
-            </div>
+              <!-- </div>
+            </div> -->
             <!-- .cs-slider_container -->
-            <div class="cs-pagination cs-style1 cs-hidden_desktop"></div>
+            <!-- <div class="cs-pagination cs-style1 cs-hidden_desktop"></div>
           </div>
-        </div>
+        </div> -->
         <!-- .cs-slider -->
-      </div>
-    </section>
+      <!-- </div> -->
+    <!-- </section> -->
     <!-- End Awards Text -->
-    <div class="cs-height_130 cs-height_lg_70" ></div>
+    <!-- <div class="cs-height_130 cs-height_lg_70" ></div> -->
     <!-- Start General Text -->
-    <div class="container" >
-      <h2 class="cs-font_50 cs-m0 text-center cs-line_height_4 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s"> 
+    <!-- <div class="container" > -->
+      <!-- <h2 class="cs-font_50 cs-m0 text-center cs-line_height_4 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s">  -->
        
-      </h2>
-    </div>
+      <!-- </h2> -->
+    <!-- </div> -->
     <!-- End General Text -->
-    <div class="cs-height_70 cs-height_lg_70" ></div>
+    <!-- <div class="cs-height_70 cs-height_lg_70" ></div> -->
     <!-- Start Video Block -->
     <!-- <div class="container" >
       <a href="https://www.youtube.com/watch?v=VcaAVWtP48A" class="cs-video_block cs-style1 cs-video_open cs-bg" data-src="assets/img/video_bg.jpeg">
@@ -590,7 +590,7 @@
           <div class="cs-slider_heading cs-style1">
             <div class="cs-section_heading cs-style1">
               <h3 class="cs-section_subtitle wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.2s"> Our Team </h3>
-              <h2 class="cs-section_title">Team <br>members </h2>
+              <h2 class="cs-section_title">Team members </h2>
             </div>
             <!-- <div class="cs-slider_arrows cs-style1 cs-primary_color">
               <div class="cs-left_arrow cs-center">
