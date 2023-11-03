ca7d2692e69b83e9cf813430f78e4b9e3286f43b
AljoAntony24 <aljoantony24@gmail.com>
2023-11-03 10:50:55 +0530

    new file

--- index.php
+++ index.php
@@ -36,287 +36,14 @@
     </div>
     <!-- Start Header Section -->
     <?php include("includes/header.php") ?>
-    
-    <!-- End Header Section -->
-    <!-- Start Hero -->
-    
-    <!-- <div class="cs-hero cs-style1 cs-bg cs-fixed_bg cs-shape_wrap_1" data-src="assets/img/bg2.jpeg" id="home" 
-    style="height: fit-content; justify-content: center;">  -->
-
-    <div class="carousel">
-      <input type="radio" name="carousel" id="slide-btn-1" class="slide-btn" onclick="setInt();" checked />
-      <input type="radio" name="carousel" id="slide-btn-2" class="slide-btn" onclick="setInt();" />
-      <input type="radio" name="carousel" id="slide-btn-3" class="slide-btn" onclick="setInt();" />
-      <input type="radio" name="carousel" id="slide-btn-4" class="slide-btn" onclick="setInt();" />
-      <input type="radio" name="carousel" id="slide-btn-5" class="slide-btn" onclick="setInt();" />
-      <!-- <div class="slide one parallax-effect" style="background-image: url('assets/img/bg2.jpeg');">
-        
-        
-        </div> -->
-      <!-- <div class="" style="background-repeat: no-repeat; background-size: cover; background-image: url('assets/img/flo.jpeg');">
-        <h1>Slide 2</h1>
-        </div> -->
-      <!-- <video   autoplay controls loop muted style="width:1550px;">
-        <source src="new.mp4" type="video/mp4" playsinline playsinline >
-       
-      </video> -->
-      <video  autoplay controls loop muted style="width:1550px; height: auto;">
-        <source src="assets/img/videos/slider-1.mp4" type="video/mp4" playsinline > 
-      </video>
-
-  
-  <!-- <h1 >THIS IS A RIVER.</h1>
-  <h2>How majestic.</h2> -->
-      <!-- <h1>THIS IS A RIVER.</h1> -->
-  <!-- <h2>How majestic.</h2> -->
-      <!-- <div class="labels">
-        <label for="slide-btn-1"></label>
-        <label for="slide-btn-2"></label>
-        <label for="slide-btn-3"></label>
-        
-      </div> -->
-    </div>
-    <script type="text/javascript" src="script.js"></script>
-    
-       
-
-
-      <!-- <div class="firework" id="firework1">
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
+   
+      <div class="cs-hero " style="position:relative;" id="sound_volume">
+        <video autoplay muted loop id="myVideo" style="width: 1550px; height: auto;">
+          <source src="assets/img/videos/slider-1.mp4" type="video/mp4" /> 
+        </video>
+        <i class="fa-solid fa-volume-off sound" style="bottom:170px ; right: 50px; font-size: 30px; color: #ff3399; position: absolute; "></i>
       </div>
-      <div class="firework" id="firework2">
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-      </div>
-      <div class="firework" id="firework3">
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-      </div>
-
-      <div class="firework" id="firework4">
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-        <div class="explosion"></div>
-      </div> -->
-      <!-- <div class="cs-shape_1"></div>
-=======
-      <div class="cs-shape_1"></div>
-      <div class="cs-shape_1"></div>
->>>>>>> 4d77b3c705afaeb6002284fd67c681bc9e76a88b
-      <div class="cs-shape_1"></div>
-      <div class="cs-shape_1"></div> -->
-      <div class="container">
-        <!-- <div class="cs-hero_text">
-          
-          <h1 class="cs-hero_title wow fadeInRight" data-wow-duration="0.8s" data-wow-delay="0.2s">  <br> </h1>
-          <div class="cs-hero_info">
-            <div>
-              <a href="contact.html" class="cs-text_btn">
-                <span>Get a Quote</span>
-                <svg width="26" height="12" viewBox="0 0 26 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M25.5303 6.53033C25.8232 6.23744 25.8232 5.76256 25.5303 5.46967L20.7574 0.696699C20.4645 0.403806 19.9896 0.403806 19.6967 0.696699C19.4038 0.989593 19.4038 1.46447 19.6967 1.75736L23.9393 6L19.6967 10.2426C19.4038 10.5355 19.4038 11.0104 19.6967 11.3033C19.9896 11.5962 20.4645 11.5962 20.7574 11.3033L25.5303 6.53033ZM0 6.75H25V5.25H0V6.75Z" fill="currentColor" />
-                </svg>
-              </a>
-            </div>
-            <div>
-              <div class="cs-hero_subtitle"> </div>
-            </div>
-          </div>
-        </div>
-      </div> -->
-      <!-- <div class="cs-hero_social_wrap cs-primary_font cs-primary_color">
-        <div class="cs-hero_social_title">Follow Us</div>
-        <ul class="cs-hero_social_links">
-          <li>
-            <a href="">Behance</a>
-          </li>
-          <li>
-            <a href="">Twitter</a>
-          </li>
-        </ul>
-      </div> -->
-      <a href="#service" class="cs-down_btn"></a>
-    </div>
-    <!-- End Hero -->
-    <!-- Start FunFact -->
-    <!-- <section>
-      <div class="container">
-        <div class="cs-funfact_wrap cs-type1">
-          <div class="cs-funfact_shape" data-src="assets/img/funfact_shape_bg.svg"></div>
-          <div class="cs-funfact_left">
-            <div class="cs-funfact_heading">
-              <h2>Forging Ahead in Our Tale of Accomplishments!</h2>
-            </div>
-          </div>
-          <div class="cs-funfact_right">
-            <div class="cs-funfacts">
-              <div class="cs-funfact cs-style1">
-                <div class="cs-funfact_number cs-primary_font cs-semi_bold cs-primary_color">
-                  <span data-count-to="3" class="odometer"></span>K
-                </div>
-                <div class="cs-funfact_text">
-                  <span class="cs-accent_color">+</span>
-                  <p>Operational Years</p>
-                </div>
-              </div>
-              <div class="cs-funfact cs-style1">
-                <div class="cs-funfact_number cs-primary_font cs-semi_bold cs-primary_color">
-                  <span data-count-to="4" class="odometer"></span>
-                </div>
-                <div class="cs-funfact_text">
-                  <span class="cs-accent_color">+</span>
-                  <p>Countries</p>
-                </div>
-              </div>
-              <div class="cs-funfact cs-style1">
-                <div class="cs-funfact_number cs-primary_font cs-semi_bold cs-primary_color">
-                  <span data-count-to="21" class="odometer"></span>
-                </div>
-                <div class="cs-funfact_text">
-                  <span class="cs-accent_color">+</span>
-                  <p>Satisfied Clients</p>
-                </div>
-              </div>
-              <div class="cs-funfact cs-style1">
-                <div class="cs-funfact_number cs-primary_font cs-semi_bold cs-primary_color">
-                  <span data-count-to="63" class="odometer"></span>
-                </div>
-                <div class="cs-funfact_text">
-                  <span class="cs-accent_color">+</span>
-                  <p>Solution Delivered</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section> -->
-    <!-- End FunFact -->
-    <!-- Start Service Section -->
-    <!-- <section id="service" style="background-image: url('assets/img/sbg6.png');  background-size: cover;
-    background-repeat: no-repeat; background-position: bottom center;">
-      <div class="cs-height_150 cs-height_lg_80"></div>
-      <div class="container" >
-        <div class="row" >
-          <div class="col-xl-4">
-            <div class="cs-section_heading cs-style1">
-              <h3 class="cs-section_subtitle">What Can We Do</h3>
-              <h2 class="cs-section_title">Services we can help you with</h2>
-              <div class="cs-height_45 cs-height_lg_20"></div>
-              <a href="service.html" class="cs-text_btn wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.2s">
-                <span>See All Services</span>
-                <svg width="26" height="12" viewBox="0 0 26 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M25.5303 6.53033C25.8232 6.23744 25.8232 5.76256 25.5303 5.46967L20.7574 0.696699C20.4645 0.403806 19.9896 0.403806 19.6967 0.696699C19.4038 0.989593 19.4038 1.46447 19.6967 1.75736L23.9393 6L19.6967 10.2426C19.4038 10.5355 19.4038 11.0104 19.6967 11.3033C19.9896 11.5962 20.4645 11.5962 20.7574 11.3033L25.5303 6.53033ZM0 6.75H25V5.25H0V6.75Z" fill="#ff3399"></path>
-                </svg>
-              </a>
-            </div>
-            <div class="cs-height_90 cs-height_lg_45"></div>
-          </div>
-          <div class="col-xl-8">
-            <div class="row">
-              <div class="col-lg-3 col-sm-6 cs-hidden_mobile"></div>
-              <div class="col-lg-3 col-sm-6">
-                <div class="cs-hobble">
-                  <a href="#" class="cs-card cs-style1 cs-hover_layer1">
-                    <img src="assets/img/service_1.jpeg" alt="Service">
-                    <div class="cs-card_overlay"></div>
-                    <div class="cs-card_info">
-                      <span class="cs-hover_layer3 cs-accent_bg"></span>
-                      <h2 class="cs-card_title">UI/UX design</h2>
-                    </div>
-                  </a>
-                </div>
-                <div class="cs-height_0 cs-height_lg_30"></div>
-              </div>
-              <div class="col-lg-3 col-sm-6 cs-hidden_mobile"></div>
-              <div class="col-lg-3 col-sm-6">
-                <div class="cs-hobble">
-                  <a href="#" class="cs-card cs-style1 cs-hover_layer1">
-                    <img src="assets/img/service_2.jpeg" alt="Service">
-                    <div class="cs-card_overlay"></div>
-                    <div class="cs-card_info">
-                      <span class="cs-hover_layer3 cs-accent_bg"></span>
-                      <h2 class="cs-card_title">React.js Development</h2>
-                    </div>
-                  </a>
-                </div>
-                <div class="cs-height_0 cs-height_lg_30"></div>
-              </div>
-              <div class="col-lg-3 col-sm-6">
-                <div class="cs-hobble">
-                  <a href="#" class="cs-card cs-style1 cs-hover_layer1">
-                    <img src="assets/img/service_3.jpeg" alt="Service">
-                    <div class="cs-card_overlay"></div>
-                    <div class="cs-card_info">
-                      <span class="cs-hover_layer3 cs-accent_bg"></span>
-                      <h2 class="cs-card_title">Digital Marketing</h2>
-                    </div>
-                  </a>
-                </div>
-                <div class="cs-height_0 cs-height_lg_30"></div>
-              </div>
-              <div class="col-lg-3 col-sm-6 cs-hidden_mobile"></div>
-              <div class="col-lg-3 col-sm-6">
-                <div class="cs-hobble">
-                  <a href="#" class="cs-card cs-style1 cs-hover_layer1">
-                    <img src="assets/img/service_4.jpeg" alt="Service">
-                    <div class="cs-card_overlay"></div>
-                    <div class="cs-card_info">
-                      <span class="cs-hover_layer3 cs-accent_bg"></span>
-                      <h2 class="cs-card_title">Technology</h2>
-                    </div>
-                  </a>
-                </div>
-                <div class="cs-height_0 cs-height_lg_30"></div>
-              </div>
-              <div class="col-lg-3 col-sm-6 cs-hidden_mobile"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="cs-height_150 cs-height_lg_50"></div>
-    </section> -->
-    <!-- End Service Section -->
+      
 
 <!-- about us  -->
 
@@ -1397,6 +1124,30 @@
     </div>
     <!-- End Video Popup -->
     <!-- Script -->
+    <script>
+      const sound = document.querySelector(".sound");
+      const video = document.querySelector("#myVideo");
+  
+      sound.addEventListener("click", function(){
+          if(video.muted){
+              video.muted = false;
+              sound.classList.remove("fa-volume-off");
+              sound.classList.add("fa-volume-up");
+          } else {
+              video.muted = true;
+              sound.classList.remove("fa-volume-up");
+              sound.classList.add("fa-volume-off");
+          }
+      });
+  
+     window.addEventListener("scroll", function(){
+        if (window.scrollY >= 1500) {
+            video.muted = true;
+            sound.classList.remove("fa-volume-up");
+            sound.classList.add("fa-volume-off");
+        }
+    });
+  </script>
     <script src="assets/js/plugins/jquery-3.6.0.min.js"></script>
     <script src="assets/js/plugins/isotope.pkg.min.js"></script>
     <script src="assets/js/plugins/jquery.slick.min.js"></script>
